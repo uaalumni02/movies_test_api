810b74a188bd1961077d39e09be871d5bb4a5bbb
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-11-04 19:28:01 -0400

    test post request

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'Movies API', types: :request do
+    describe 'GET /movies' do
   it 'return all moveis' do
     FactoryBot.create(:movie, name: 'Coming to America' )
     FactoryBot.create(:movie, name: 'Boyz in the Hood' )
@@ -9,5 +10,12 @@ describe 'Movies API', types: :request do
 
     expect(response).to have_http_status(:success)
     expect(JSON.parse(response.body).size).to eq(2)
-   end
+    end
+  end
+
+  describe 'POST /movies' do
+      it 'creates a new movie'do
+      post '/movies', params: {movie: {name: 'Best Man'}}
+    end
+  end
 end
