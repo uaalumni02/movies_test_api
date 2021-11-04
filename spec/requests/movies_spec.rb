f7265cd866a868f42ac4d0cc6da6f9f4ea21b629
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-11-04 19:14:39 -0400

    get request test

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe 'Movies API', types: :request do
   it 'return all moveis' do
+    FactoryBot.create(:movie, name: 'Coming to America' )
+    FactoryBot.create(:movie, name: 'Boyz in the Hood' )
+
     get '/movies'
 
     expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(3)
+    expect(JSON.parse(response.body).size).to eq(2)
    end
 end
